f4d807d8f956ed8a227833dd76e41a9b9cbd8c92
Sarah Maas <sarah.ann.maas@gmail.com>
2018-05-29 14:43:07 -0500

    add constants file

---
+++ src/app/app.constants.ts
@@ -0,0 +1,17 @@
+export const appConfig = {
+  userLocalStorage: 'user',
+  resourceAccessLocalStorage: 'resourceAccessRaw',
+  accessTokenServer: 'X-Auth-Token',
+  defaultContentTypeHeader: 'application/json',
+  loginPageUrl: '/login',
+  registrationPageUrl: '/register',
+  errorInputClass: 'has-error',
+  successInputClass: 'has-success',
+  actionSearchKey: 'Entity',
+  resourceActions: {
+    getActionName: 'Read',
+    addActionName: 'Create',
+    updateActionName: 'Update',
+    deleteActionName: 'Delete',
+  }
+};
