91866f89509ef938f50916e829ba9e85e583dd87
Sarah Maas <sarah.ann.maas@gmail.com>
2018-05-30 18:34:44 -0500

    add more outes to recipes

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -24,33 +24,54 @@ export class RecipeService {
   }
 
   // GET /users/:id/recipes
-  public getUserRecipes() {
-    
+  public getUserRecipes(userID: number): Observable<Recipe[]> {
+    return this.authTokenService
+    .get('/users/' + userID + '/recipes').map(
+      res => {
+        const recipes = res.json();
+        return recipes.map((recipe) => new Recipe(recipe));
+      }
+    ).catch(this.handleError);
   }
 
   // GET /users/:user_id/recipes/:id
   public getUserRecipeById(recipeID: number) {
-
+    return this.authTokenService.get('/users/' + this.userID + '/recipes/' + recipeID)
+    .map(
+      res => {
+        return new Recipe(res.json());
+      }
+    ).catch(this.handleError);
   } 
 
   // GET /recipes --> gets all recipes
   public getAllRecipes(): Observable<Recipe[]> {
     return this.authTokenService.get('/recipes').map(
-      res =>{
+      res => {
         const recipes = res.json();
         return recipes.map((recipe) => new Recipe(recipe));
     }).catch(this.handleError);
   }
 
   // POST /users/:id/recipes
-  public createRecipe(recipe: Recipe) {
-
+  public createRecipe(recipe: Recipe): Observable<Recipe> {
+    return this.authTokenService.post('/users/' + this.userID + '/recipes', recipe)
+    .map(
+      res => {
+        return new Recipe(res.json());
+      }
+    ).catch(this.handleError);
   }
 
   // PUT /users/:user_id/recipes/:id
-  public updateRecipe(recipe: Recipe) {
-
-  }
+  // id is not yet an attribute of recipe model
+  // public updateRecipe(recipe: Recipe): Observable<Recipe> {
+  //   return this.authTokenService.put('/users/' + this.userID + '/recipes/' + recipe.id, recipe).map(
+  //     res => {
+  //       return new Recipe(res.json());
+  //     }
+  //   ).catch(this.handleError);
+  // }
 
   // DELETE /recipes/:id 
   public deleteRecipe(recipeID: number) {
