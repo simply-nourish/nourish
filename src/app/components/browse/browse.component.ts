8363f18fdac68c2f311f74ca3b21b1564ffda3e8
Sarah Maas <sarah.ann.maas@gmail.com>
2018-06-06 09:52:53 -0500

    formatting

--- src/app/components/browse/browse.component.ts
+++ src/app/components/browse/browse.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-import {RecipeService} from "../../services/recipe.service";
-import {Recipe} from '../../models/Recipe';
+import { RecipeService } from "../../services/recipe.service";
+import { Recipe } from '../../models/Recipe';
 
 @Component({
   selector: 'app-browse',
